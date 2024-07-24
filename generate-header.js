29f8dee4c62c2337fdd5f0c83069dec41a703cb4
Liu, Xiaoyi <xiaoyi.liu@intel.com>
2024-07-24 21:31:32 +0800

    Add script to generate header from state file

---
+++ generate-header.js
@@ -0,0 +1,42 @@
+const process = require('process');
+const fs = require('fs');
+
+const fn = process.argv[2];
+if(fn === undefined) {
+  console.error('Usage: node generate-header.js ./state.json');
+  process.exit(1);
+}
+const states = JSON.parse(fs.readFileSync(fn).toString('UTF-8'));
+console.error(`Processing ${fn}`);
+
+function type_lookup(len) {
+  if(len > 64) throw new Error(`Bit len too large: ${len}`);
+
+  if(len === 1) return 'bool';
+  if(len <= 8) return 'uint8_t';
+  if(len <= 16) return 'uint16_t';
+  if(len <= 32) return 'uint32_t';
+  return 'uint64_t';
+}
+
+function state_name(state) {
+  return `${state.type === 'input' ? 'i' : 'o'}_${state.name}`;
+}
+
+let output = `// Auto-generated from ${fn}\n`;
+for(const mod of states) {
+  const struct_name = mod.name + '_state';
+  let struct = `struct ${struct_name} {\n`;
+  for(const state of mod.states) {
+    const ty = type_lookup(state.numBits);
+    struct += `  ${ty} ${state_name(state)};\n`;
+  }
+  struct += `};\n`
+  struct += `static_assert(sizeof(${struct_name}) == ${mod.numStateBytes});`
+  for(const state of mod.states)
+    struct += `static_assert(offsetof(${struct_name}, ${state_name(state)}) == ${state.offset});\n`;
+
+  output += struct;
+}
+
+console.log(output);
